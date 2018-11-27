9a58d3914f8ae21a8dd5942e8705155fe7efc679
WillXusd <33019963+WillXusd@users.noreply.github.com>
2018-11-27 14:37:39 +0800

    模板式表单

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
