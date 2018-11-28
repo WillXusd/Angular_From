b06c03d6200d40ef22db651d81f0ee0b7752c2fc
WillXusd <33019963+WillXusd@users.noreply.github.com>
2018-11-28 11:37:55 +0800

    validator in the view

--- src/app/reactive-regist/reactive-regist.component.ts
+++ src/app/reactive-regist/reactive-regist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, AbstractControl, Validators } from '@angular/forms';
+import { equalValidator, mobileValidator } from '../validator/validators';
 
 @Component({
   selector: 'app-reactive-regist',
@@ -12,21 +13,7 @@ export class ReactiveRegistComponent implements OnInit {
   //XXXXX(control: AbstractControl): { [key: string]: any } {
   //  return null;
   //}
-  mobileValidator(control: FormControl): any
- {
-  var myreg = /^1[34578]\d{9}$/;
-  let valid = myreg.test(control.value);
-  console.log("mobile的校验结果是:" + valid);
-  return valid ? null : { mobile: true };//校验器返回null表示校验通过
-  }
-  //formGroup的校验器
-  equalValidator(group: FormGroup): any {
-    let password: FormControl = group.get("password") as FormControl;
-    let pconfirm: FormControl = group.get("pconfirm") as FormControl;
-    let valid: boolean = (password.value === pconfirm.value);
-    console.log("密码校验结果->",valid);
-    return valid ? null : { equal: true };
-  }
+
 
 
   formModel: FormGroup;
@@ -46,11 +33,11 @@ export class ReactiveRegistComponent implements OnInit {
   constructor(fb:FormBuilder) {
     this.formModel = fb.group({
       username: ['', [Validators.required, Validators.minLength(6)]],//两个参数，第一个是默认值，第二个是校验器
-      mobile: ['', this.mobileValidator],
+      mobile: ['',mobileValidator],
       passwordsGroup: fb.group({
-        password: [''],
+        password: ['',Validators.minLength(6)], 
         pconfirm: ['']
-      }, {validator:this.equalValidator})
+      }, {validator:equalValidator})
     })
   }
 
@@ -59,7 +46,10 @@ export class ReactiveRegistComponent implements OnInit {
     console.log("校验结果-》", isValid);
     let errors: any = this.formModel.get("username").errors;
     console.log("校验信息-》", errors);
-    console.log(this.formModel.value);
+   
+    if (this.formModel.valid) {
+      console.log("验证成功->",this.formModel.value);
+    }
   }
 
   ngOnInit() {
---
+++ src/app/validator/validators.ts
@@ -0,0 +1,28 @@
+import { FormControl, FormGroup } from "@angular/forms";
+import { Observable } from "rxjs";
+
+//普通校验器
+export function mobileValidator(control: FormControl): any
+{
+  var myreg = /^1[34578]\d{9}$/;
+  let valid = myreg.test(control.value);
+  console.log("mobile的校验结果是:" + valid);
+  return valid ? null : { mobile: true };//校验器返回null表示校验通过
+}
+
+//异步校验器
+//export function mobileAsyncValidator(control: FormControl): any {
+//  var myreg = /^1[34578]\d{9}$/;
+//  let valid = myreg.test(control.value);
+//  console.log("mobile的校验结果是:" + valid);
+//  return Observable.of(valid ? null : { mobile: true });
+//}
+//formGroup的校验器
+export function equalValidator(group: FormGroup): any {
+  let password: FormControl = group.get("password") as FormControl;
+  let pconfirm: FormControl = group.get("pconfirm") as FormControl;
+  let valid: boolean = (password.value === pconfirm.value);
+  console.log("密码校验结果->", valid);
+  return valid ? null : {
+    equal: {desc:"密码和确认密码不匹配"} };
+}
